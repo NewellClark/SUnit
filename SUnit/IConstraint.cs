914e130919b12b5dd59b56740d750afeb69c5360
Newell Clark <newellofthefuture@gmail.com>
2020-03-07 14:34:26 -0500

    Added IConstraint<T>

---
+++ SUnit/IConstraint.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SUnit
+{
+    /// <summary>
+    /// A constraint that can be applied to value. 
+    /// </summary>
+    /// <typeparam name="T">The type of value the constraint can be applied to.</typeparam>
+    public interface IConstraint<T>
+    {
+        /// <summary>
+        /// Applies the constraint to the specified value.
+        /// </summary>
+        /// <param name="value">The value to apply the constraint to.</param>
+        /// <returns>Whether the value satisfies the constraint.</returns>
+        public bool Apply(T value);
+
+
+        private sealed class NotConstraint : IConstraint<T>
+        {
+            private readonly IConstraint<T> inner;
+            public NotConstraint(IConstraint<T> inner)
+            {
+                this.inner = inner;
+            }
+
+            public bool Apply(T value) => !inner.Apply(value);
+        }
+
+        public static IConstraint<T> operator !(IConstraint<T> operand)
+        {
+            if (operand is null) throw new ArgumentNullException(nameof(operand));
+
+            return new NotConstraint(operand);
+        }
+
+        private abstract class BinaryConstraint : IConstraint<T>
+        {
+            private readonly IConstraint<T> left;
+            private readonly IConstraint<T> right;
+
+            public BinaryConstraint(IConstraint<T> left, IConstraint<T> right)
+            {
+                this.left = left;
+                this.right = right;
+            }
+
+            protected abstract bool ApplyToBoth(T value, IConstraint<T> left, IConstraint<T> right);
+
+            public bool Apply(T value) => ApplyToBoth(value, left, right);
+        }
+
+        private sealed class AndConstraint : BinaryConstraint
+        {
+            public AndConstraint(IConstraint<T> left, IConstraint<T> right) : base(left, right) { }
+            protected override bool ApplyToBoth(T value, IConstraint<T> left, IConstraint<T> right)
+            {
+                return left.Apply(value) & right.Apply(value);
+            }
+        }
+        
+        public static IConstraint<T> operator &(IConstraint<T> left, IConstraint<T> right)
+        {
+            if (left is null) throw new ArgumentNullException(nameof(left));
+            if (right is null) throw new ArgumentNullException(nameof(right));
+
+            return new AndConstraint(left, right);
+        }
+
+        private sealed class OrConstraint : BinaryConstraint
+        {
+            public OrConstraint(IConstraint<T> left, IConstraint<T> right) : base(left, right) { }
+            protected override bool ApplyToBoth(T value, IConstraint<T> left, IConstraint<T> right)
+            {
+                return left.Apply(value) | right.Apply(value);
+            }
+        }
+
+        public static IConstraint<T> operator |(IConstraint<T> left, IConstraint<T> right)
+        {
+            if (left is null) throw new ArgumentNullException(nameof(left));
+            if (right is null) throw new ArgumentNullException(nameof(right));
+
+            return new OrConstraint(left, right);
+        }
+
+        private sealed class XorConstraint : BinaryConstraint
+        {
+            public XorConstraint(IConstraint<T> left, IConstraint<T> right) : base(left, right) { }
+
+            protected override bool ApplyToBoth(T value, IConstraint<T> left, IConstraint<T> right)
+            {
+                return left.Apply(value) ^ right.Apply(value);
+            }
+        }
+
+        public static IConstraint<T> operator ^ (IConstraint<T> left, IConstraint<T> right)
+        {
+            if (left is null) throw new ArgumentNullException(nameof(left));
+            if (right is null) throw new ArgumentNullException(nameof(right));
+
+            return new XorConstraint(left, right);
+        }
+    }
+}
--- SUnitTests/AssertionTests.cs
+++ SUnitTests/AssertionTests.cs
@@ -6,123 +6,131 @@ using assert = NUnit.Framework.Assert;
 
 namespace SUnit
 {
-    [TestFixture]
-    public class AssertionTests
+    public class Temp
     {
         [Test]
-        public void EqualDoubles_Equal_Passes()
+        public void Experimenting()
         {
-            assert.That(Assert.That(0.0).Is.EqualTo(0.0).Passed, Is.True);
-        }
-
-        [Test]
-        public void UnequalDoubles_Equal_Fails()
-        {
-            assert.That(Assert.That(0.0).Is.EqualTo(0.1).Passed, Is.False);
-        }
-
-        [Test]
-        public void EqualDoubles_NotEqual_Fails()
-        {
-            assert.That(Assert.That(0.0).Is.Not.EqualTo(0.0).Passed, Is.False);
-        }
-
-        [Test]
-        public void NullReference_IsNull()
-        {
-            object @null = null;
-            assert.That(Assert.That(@null).Is.Null.Passed, Is.True);
-        }
-
-        [Test]
-        public void NullNullableValue_IsNull()
-        {
-            double? @null = null;
-            assert.That(Assert.That(@null).Is.Null.Passed, Is.True);
-        }
-
-        [Test]
-        public void NonNullableValue_IsNotNull()
-        {
-            assert.That(Assert.That(17).Is.Null.Passed, Is.False);
-        }
-
-        [Test]
-        public void NonNullReference_IsNotNull()
-        {
-            assert.That(Assert.That(new object()).Is.Null.Passed, Is.False);
-        }
-
-        [Test]
-        public void True_IsTrue()
-        {
-            assert.That(Assert.That(true).Is.True.Passed, Is.True);
-        }
-
-        [Test]
-        public void False_IsNotTrue()
-        {
-            assert.That(Assert.That(false).Is.True.Passed, Is.False);
-        }
-
-        [Test]
-        public void True_IsNotFalse()
-        {
-            assert.That(Assert.That(true).Is.False.Passed, Is.False);
-        }
-
-        [Test]
-        public void False_IsFalse()
-        {
-            assert.That(Assert.That(false).Is.False.Passed, Is.True);
-        }
-
-        [Test]
-        public void LessThan_LesserValue_Pass()
-        {
-            assert.That(Assert.That(5).Is.LessThan(6).Passed, Is.True);
-        }
-
-        [Test]
-        public void LessThan_EqualValues_Fails()
-        {
-            assert.That(Assert.That(5).Is.LessThan(5).Passed, Is.False);
-        }
-
-        [Test]
-        public void LessThanOrEqualTo_EqualValues_Passes()
-        {
-            assert.That(Assert.That(5).Is.LessThanOrEqualTo(5).Passed, Is.True);
-        }
-
-        [Test]
-        public void LessThanOrEqualTo_GreaterValue_Fails()
-        {
-            assert.That(Assert.That(5).Is.LessThanOrEqualTo(4).Passed, Is.False);
-        }
-
-        [Test]
-        public void GreaterThan_GreaterValue_Pass()
-        {
-            assert.That(Assert.That(-1.4).Is.GreaterThan(-1.5).Passed, Is.True);
-        }
-
-        [Test]
-        public void GreaterThan_EqualValue_Fails()
-        {
-            assert.That(Assert.That(16.3).Is.GreaterThan(19.001).Passed, Is.False);
-        }
-
-        [Test]
-        public void GreaterThanOrEqualTo_EqualValue_Pass()
-        {
-            assert.That(Assert.That(5).Is.GreaterThanOrEqualTo(5).Passed, Is.True);
-        }
-
-        [Test]
-        public void GreaterThanOrEqualTo_LesserValue_Fails()
-        {
-            assert.That(Assert.That(-123).Is.GreaterThanOrEqualTo(11).Passed, Is.False);
+            assert.That(5, Is.EqualTo(7).Or.EqualTo(8).Or.EqualTo(5).And.EqualTo(7));
         }
     }
+    //[TestFixture]
+    //public class AssertionTests
+    //{
+    //    [Test]
+    //    public void EqualDoubles_Equal_Passes()
+    //    {
+    //        assert.That(Assert.That(0.0).Is.EqualTo(0.0).Passed, Is.True);
+    //    }
+
+    //    [Test]
+    //    public void UnequalDoubles_Equal_Fails()
+    //    {
+    //        assert.That(Assert.That(0.0).Is.EqualTo(0.1).Passed, Is.False);
+    //    }
+
+    //    [Test]
+    //    public void EqualDoubles_NotEqual_Fails()
+    //    {
+    //        assert.That(Assert.That(0.0).Is.Not.EqualTo(0.0).Passed, Is.False);
+    //    }
+
+    //    [Test]
+    //    public void NullReference_IsNull()
+    //    {
+    //        object @null = null;
+    //        assert.That(Assert.That(@null).Is.Null.Passed, Is.True);
+    //    }
+
+    //    [Test]
+    //    public void NullNullableValue_IsNull()
+    //    {
+    //        double? @null = null;
+    //        assert.That(Assert.That(@null).Is.Null.Passed, Is.True);
+    //    }
+
+    //    [Test]
+    //    public void NonNullableValue_IsNotNull()
+    //    {
+    //        assert.That(Assert.That(17).Is.Null.Passed, Is.False);
+    //    }
+
+    //    [Test]
+    //    public void NonNullReference_IsNotNull()
+    //    {
+    //        assert.That(Assert.That(new object()).Is.Null.Passed, Is.False);
+    //    }
+
+    //    [Test]
+    //    public void True_IsTrue()
+    //    {
+    //        assert.That(Assert.That(true).Is.True.Passed, Is.True);
+    //    }
+
+    //    [Test]
+    //    public void False_IsNotTrue()
+    //    {
+    //        assert.That(Assert.That(false).Is.True.Passed, Is.False);
+    //    }
+
+    //    [Test]
+    //    public void True_IsNotFalse()
+    //    {
+    //        assert.That(Assert.That(true).Is.False.Passed, Is.False);
+    //    }
+
+    //    [Test]
+    //    public void False_IsFalse()
+    //    {
+    //        assert.That(Assert.That(false).Is.False.Passed, Is.True);
+    //    }
+
+    //    [Test]
+    //    public void LessThan_LesserValue_Pass()
+    //    {
+    //        assert.That(Assert.That(5).Is.LessThan(6).Passed, Is.True);
+    //    }
+
+    //    [Test]
+    //    public void LessThan_EqualValues_Fails()
+    //    {
+    //        assert.That(Assert.That(5).Is.LessThan(5).Passed, Is.False);
+    //    }
+
+    //    [Test]
+    //    public void LessThanOrEqualTo_EqualValues_Passes()
+    //    {
+    //        assert.That(Assert.That(5).Is.LessThanOrEqualTo(5).Passed, Is.True);
+    //    }
+
+    //    [Test]
+    //    public void LessThanOrEqualTo_GreaterValue_Fails()
+    //    {
+    //        assert.That(Assert.That(5).Is.LessThanOrEqualTo(4).Passed, Is.False);
+    //    }
+
+    //    [Test]
+    //    public void GreaterThan_GreaterValue_Pass()
+    //    {
+    //        assert.That(Assert.That(-1.4).Is.GreaterThan(-1.5).Passed, Is.True);
+    //    }
+
+    //    [Test]
+    //    public void GreaterThan_EqualValue_Fails()
+    //    {
+    //        assert.That(Assert.That(16.3).Is.GreaterThan(19.001).Passed, Is.False);
+    //    }
+
+    //    [Test]
+    //    public void GreaterThanOrEqualTo_EqualValue_Pass()
+    //    {
+    //        assert.That(Assert.That(5).Is.GreaterThanOrEqualTo(5).Passed, Is.True);
+    //    }
+
+    //    [Test]
+    //    public void GreaterThanOrEqualTo_LesserValue_Fails()
+    //    {
+    //        assert.That(Assert.That(-123).Is.GreaterThanOrEqualTo(11).Passed, Is.False);
+    //    }
+    //}
 }
