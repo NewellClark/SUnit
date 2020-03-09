2e51b16981f36520d8582f3c4248088aa6999f4e
Newell Clark <newellofthefuture@gmail.com>
2020-03-08 21:00:56 -0400

    Added a bunch of "Things". Literally. I'll rename them later.

---
+++ SUnit/ActualValues/ThingTestBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SUnit.ActualValues
+{
+    internal abstract class ThingTestBase<T, TThing, TTest> : IThingTest<T, TThing, TTest>
+        where TThing : IThing<T, TThing, TTest>
+        where TTest : IThingTest<T, TThing, TTest>
+    {
+        private readonly T actual;
+        private readonly IConstraint<T> constraint;
+
+        protected private ThingTestBase(T actual, IConstraint<T> constraint)
+        {
+            Debug.Assert(constraint != null);
+
+            this.actual = actual;
+            this.constraint = constraint;
+        }
+
+        IConstraint<T> IThingTest<T, TThing, TTest>.Constraint => constraint;
+
+        protected private abstract TThing CreateThing(T actual, ConstraintModifier<T> constraintModifier);
+
+        public TThing ApplyModifier(ConstraintModifier<T> constraintModifier)
+        {
+            return CreateThing(actual, constraintModifier);
+        }
+    }
+}
