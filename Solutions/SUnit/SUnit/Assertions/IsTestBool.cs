1c1706e0795b2837c19a255e0749a095571b6514
NewellClark <newellofthefuture@gmail.com>
2020-03-11 03:36:32 -0400

    Auto stash before merge of "MegaFeature_NugetPackage" and "separateSolutionForTesting"

---
+++ Solutions/SUnit/SUnit/Assertions/IsTestBool.cs
@@ -0,0 +1,18 @@
+﻿using SUnit.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SUnit.Assertions
+{
+    public class IsTestBool : ActualValueTest<bool?, IIsExpressionBool, IsTestBool>
+    {
+        internal IsTestBool(bool? actual, IConstraint<bool?> constraint)
+            : base(actual, constraint) { }
+
+        protected private override IIsExpressionBool CreateExpression(bool? actual, ConstraintModifier<bool?> constraintModifier)
+        {
+            return new IsExpressionBool(actual, constraintModifier);
+        }
+    }
+}
