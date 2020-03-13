1a29315bc059b61aa0347a3b39d2a46950f3463c
Newell Clark <newellofthefuture@gmail.com>
2020-03-12 21:42:35 -0400

    Added TraitPair: a simple struct that stores a key-value pair of strings in a single string.

--- Solutions/SUnit/SUnit.Discovery/Fixture.cs
+++ Solutions/SUnit/SUnit.Discovery/Fixture.cs
@@ -6,36 +6,6 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-/*  Everything here must be:
- *  -   Not concerned with actually RUNNING tests.
- *  -   Easily serializable to text.
- *  
- *  A Fixture can:
- *  -   Tell you its Assembly.
- *  -   Tell you its Type.
- *  -   Tell you its TestMethods.
- *  -   Tell you all its Factories.
- *  -   Be round-trip serialized to text.
- *  -   Use value-equality semantics.
- *  
- *  A Factory can:
- *  -   Tell you its' Fixture's type (not its fixture!).
- *  -   Instantiate an instance of the Fixture type (not an instance of the Fixture class!). 
- *  -   Be round-trip serialized to text.
- *  -   Use value-equality semantics.
- *  
- *  A TestMethod can:
- *  -   Tell you its' Fixture's type. 
- *  -   Tell you what kind of test it is (once we add multi-tests and async tests). 
- *  -   Give you the MethodInfo for the test method it encapsulates.
- *  -   Be round-trip serialized to text.
- *  -   Use value-equality semantics.
- *  
- *  A UnitTest can:
- *  -   Tell you its' Fixture's Type.
- *  -   Tell you its' Factory.
- *  -   Tell you its' TestMethod.
- * */
 namespace SUnit.Discovery
 {
     internal class Fixture
---
+++ Solutions/SUnit/SUnit.Discovery/TraitPair.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SUnit.Discovery
+{
+    internal struct TraitPair : IEquatable<TraitPair>
+    {
+        private static readonly Regex preamble = new Regex(
+            @"(?<nameLength>[0-9]+),(?<valueLength>[0-9]+):");
+
+        public TraitPair(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public string Name { get; }
+        public string Value { get; }
+
+        public override string ToString()
+        {
+            return $"{Name.Length},{Value.Length}:{Name}{Value}";
+        }
+
+        /// <summary>
+        /// Gets the length of the string that the TraitPair is serialized to.
+        /// </summary>
+        public int SerializedLength => ToString().Length;
+
+        public static bool Equals(TraitPair left, TraitPair right)
+        {
+            return left.Name == right.Name &&
+                left.Value == right.Value;
+        }
+        public static bool operator ==(TraitPair left, TraitPair right) => Equals(left, right);
+        public static bool operator !=(TraitPair left, TraitPair right) => !Equals(left, right);
+        public bool Equals(TraitPair other) => Equals(this, other);
+        public override bool Equals(object obj)
+        {
+            return obj is TraitPair pair && Equals(this, pair);
+        }
+        public override int GetHashCode() => (Name, Value).GetHashCode();
+
+        public static TraitPair Parse(string text, int startAt)
+        {
+            Match match = preamble.Match(text, startAt);
+
+            if (!match.Success)
+                throw new FormatException("String is not a valid TraitPair.");
+            if (match.Index != startAt)
+                throw new FormatException("TraitPair preamble match did not start at the indicated index.");
+
+            int nameLength = int.Parse(match.Groups["nameLength"].Value, CultureInfo.InvariantCulture);
+            int valueLength = int.Parse(match.Groups["valueLength"].Value, CultureInfo.InvariantCulture);
+
+            int index = match.Index + match.Length;
+            string name = text.Substring(index, nameLength);
+            string value = text.Substring(index + nameLength, valueLength);
+
+            return new TraitPair(name, value);
+        }
+
+        public static TraitPair Parse(string text) => Parse(text, 0);
+
+        public static IEnumerable<TraitPair> ParseAll(string text, int startAt)
+        {
+            if (startAt < 0)
+                throw new ArgumentOutOfRangeException(nameof(startAt), "Argument can't be negative.");
+
+            static IEnumerable<TraitPair> iterator(string text, int startAt)
+            {
+                int index = startAt;
+                while (index < text.Length)
+                {
+                    TraitPair pair = Parse(text, index);
+                    index += pair.SerializedLength;
+
+                    yield return pair;
+                }
+            }
+
+            return iterator(text, startAt);
+        }
+
+        public static IEnumerable<TraitPair> ParseAll(string text) => ParseAll(text, 0);
+    }
+}
