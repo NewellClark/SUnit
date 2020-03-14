73c9dd28f8307f48b369ca1b1e60a96b34db0a01
Newell Clark <newellofthefuture@gmail.com>
2020-03-14 16:03:25 -0400

    Added true and false operators to Test, to support the lazy versions of the logical operators.

--- Solutions/SUnit/SUnitTests/Helpers.cs
+++ Solutions/SUnit/SUnitTests/Helpers.cs
@@ -11,5 +11,16 @@ namespace SUnit
         public static void AssertPassed(Test test) => assert.That(test.Passed, Is.True);
 
         public static void AssertFailed(Test test) => assert.That(test.Passed, Is.False);
+
+        public static Func<T> With<T>(this T @this, Action sideEffects)
+        {
+            if (sideEffects is null) throw new ArgumentNullException(nameof(sideEffects));
+
+            return () =>
+            {
+                sideEffects();
+                return @this;
+            };
+        }
     }
 }
--- Solutions/SUnit/SUnitTests/TestTests.cs
+++ Solutions/SUnit/SUnitTests/TestTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 using assert = NUnit.Framework.Assert;
 
 namespace SUnit
@@ -53,5 +54,88 @@ namespace SUnit
 
         [Test]
         public void FailXorFail_Fails() => Failed(Test.Fail ^ Test.Fail);
+
+
+        [TestFixture]
+        public class LazyOperators
+        {
+            private class SideEffect
+            {
+                private readonly Func<Test> supplier;
+                public SideEffect(Test value) => supplier = value.With(() => Happened = true);
+
+                public bool Happened { get; private set; }
+                public Test Get() => supplier();
+
+            }
+
+            [SetUp]
+            public void SetUp()
+            {
+                Pass = new SideEffect(Test.Pass);
+                Fail = new SideEffect(Test.Fail);
+            }
+
+            private SideEffect Pass { get; set; } 
+            private SideEffect Fail { get; set; }
+
+            
+
+            [Test]
+            public void PassAndFail_EvaluatesBoth()
+            {
+                Test _ = Pass.Get() && Fail.Get();
+
+                assert.That(Pass.Happened && Fail.Happened, Is.True);
+            }
+
+            [Test]
+            public void FailAndPass_ShortCircuits()
+            {
+                Test _ = Fail.Get() && Pass.Get();
+
+                assert.That(Pass.Happened, Is.False);
+            }
+
+            [Test]
+            public void FailOrPass_EvaluatesBoth()
+            {
+                Test _ = Fail.Get() || Pass.Get();
+
+                assert.That(Pass.Happened, Is.True);
+            }
+
+            [Test]
+            public void PassOrFail_ShortCircuits()
+            {
+                Test _ = Pass.Get() || Fail.Get();
+
+                assert.That(Fail.Happened, Is.False);
+            }
+
+            [Test]
+            public void ShortCircuitAnd_ReturnsLeftOperand()
+            {
+                Test left = Test.Fail;
+                var right = Test.Pass.With(() => Thread.CurrentThread.Abort());
+
+                Test result = left && right();
+
+                assert.That(result, Is.SameAs(left));
+            }
+
+            [Test]
+            public void ShortCircuitOr_ReturnsLeftOperand()
+            {
+                Test left = Test.Pass;
+                var right = Test.Fail.With(() => Thread.CurrentThread.Abort());
+
+                Test result = left || right();
+
+                assert.That(result, Is.SameAs(left));
+            }
+        }
+
+
     }
 }
