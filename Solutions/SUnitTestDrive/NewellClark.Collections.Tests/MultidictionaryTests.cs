470dc5741bfee6e9c8ab9159ec12c0a712f1a9e5
Newell Clark <newellofthefuture@gmail.com>
2020-03-16 12:42:06 -0400

    Safety commit.

--- Solutions/SUnitTestDrive/NewellClark.Collections.Tests/MultidictionaryTests.cs
+++ Solutions/SUnitTestDrive/NewellClark.Collections.Tests/MultidictionaryTests.cs
@@ -16,13 +16,43 @@ namespace NewellClark.Collections.Tests
             private readonly string[] keys = new[] 
             { 
                 "hello", "world", "squishy",  
-                "squidward", "is", "number", "one",
-                "", null
+                "squidward", "is", "number", "one"
             };
 
             public Test HasCountZero() => Assert.That(dictionary.Count).Is.Zero;
 
             public Test HasNoItems() => Assert.That(dictionary).Is.Empty;
+
+            public IEnumerable<Test> Indexer_ReturnsEmptySequence()
+            {
+                return keys
+                    .Where(key => key != null)
+                    .Select(key => Assert.That(dictionary[key]).Is.Empty);
+            }
+        }
+
+        public class AfterSingleAdd
+        {
+            private readonly Multidictionary<string, string> dictionary = new Multidictionary<string, string>();
+            private readonly string key = "Hello";
+            private readonly string value = "World";
+            public AfterSingleAdd()
+            {
+                dictionary.Add(key, value);
+            }
+
+            public Test HasOneItem() => Assert.That(dictionary.Count).Is.EqualTo(1);
+
+            public Test HasSingleElement()
+            {
+                return Assert.That(dictionary.Single().Key).Is.EqualTo(key) &&
+                    Assert.That(dictionary.Single().Value).Is.SequenceEqualTo(value);
+            }
+
+            public Test IndexerFetchesElement()
+            {
+                return Assert.That(dictionary[key]).Is.SequenceEqualTo(value);
+            }
         }
     }
 }
--- Solutions/SUnitTestDrive/NewellClark.Collections/Multidictionary.cs
+++ Solutions/SUnitTestDrive/NewellClark.Collections/Multidictionary.cs
@@ -9,7 +9,16 @@ namespace NewellClark.Collections
     public class Multidictionary<TKey, TValue> 
         : IEnumerable<KeyValuePair<TKey, IEnumerable<TValue>>>
     {
-        
+        private readonly Dictionary<TKey, List<TValue>> dictionary;
+
+        public Multidictionary(IEqualityComparer<TKey> comparer)
+        {
+            if (comparer is null) throw new ArgumentNullException(nameof(comparer));
+
+            dictionary = new Dictionary<TKey, List<TValue>>(comparer);
+        }
+        public Multidictionary() : this(EqualityComparer<TKey>.Default) { }
+
         public int Count { get; private set; }
 
         public void Add(TKey key, TValue value) => throw new NotImplementedException();
@@ -22,7 +31,8 @@ namespace NewellClark.Collections
 
         public IEnumerator<KeyValuePair<TKey, IEnumerable<TValue>>> GetEnumerator()
         {
-            throw new NotImplementedException();
+            foreach (var kvp in dictionary)
+                yield return new KeyValuePair<TKey, IEnumerable<TValue>>(kvp.Key, kvp.Value);
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
