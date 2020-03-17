0144b6a2f85424c7d4beda56ac4df127a66268cd
Newell Clark <newellofthefuture@gmail.com>
2020-03-16 21:11:01 -0400

    Added SUnitTestDrive solution. Took SUnit for a "test drive" by implementing a Multidictionary<TKey, TValue>

--- Solutions/SUnitTestDrive/NewellClark.Collections.Tests/MultidictionaryTests.cs
+++ Solutions/SUnitTestDrive/NewellClark.Collections.Tests/MultidictionaryTests.cs
@@ -10,7 +10,7 @@ namespace NewellClark.Collections.Tests
     //  Any public class that contains unit tests is a "Test Fixture".
     public class MultidictionaryTests
     {
-        public class NewMultidictionary
+        public class EmptyMultidictionary
         {
             private readonly Multidictionary<string, string> dictionary = new Multidictionary<string, string>();
             private readonly string[] keys = new[] 
@@ -19,9 +19,13 @@ namespace NewellClark.Collections.Tests
                 "squidward", "is", "number", "one"
             };
 
-            public Test HasCountZero() => Assert.That(dictionary.Count).Is.Zero;
+            public Test HasEntryCountZero() => Assert.That(dictionary.Count).Is.Zero;
 
-            public Test HasNoItems() => Assert.That(dictionary).Is.Empty;
+            public Test HasKeyCountZero() => Assert.That(dictionary.Keys.Count).Is.Zero;
+
+            public Test HasNoEntries() => Assert.That(dictionary).Is.Empty;
+
+            public Test HasNoKeys() => Assert.That(dictionary.Keys).Is.Empty;
 
             public IEnumerable<Test> Indexer_ReturnsEmptySequence()
             {
@@ -29,29 +33,173 @@ namespace NewellClark.Collections.Tests
                     .Where(key => key != null)
                     .Select(key => Assert.That(dictionary[key]).Is.Empty);
             }
+
+            public IEnumerable<Test> DoesNotContainAnyKeys()
+            {
+                foreach (string key in keys)
+                    yield return Assert.That(dictionary.ContainsKey(key)).Is.False;
+            }
+
+            public IEnumerable<Test> DoesNotRemoveAnyKeys()
+            {
+                foreach (string key in keys)
+                    yield return Assert.That(dictionary.Remove(key)).Is.False;
+            }
+
+            public Test Add_ThrowsWhenKeyNull()
+            {
+                return Assert.Throws<ArgumentNullException>(() => dictionary.Add(null, "whatever"));
+            }
+
+            public Test AddRange_ThrowsWhenKeyNull()
+            {
+                return Assert.Throws<ArgumentNullException>(() => dictionary.AddRange(null, Enumerable.Empty<string>()));
+            }
+
+            public Test AddRange_ThrowsWhenCollectionNull()
+            {
+                return Assert.Throws<ArgumentNullException>(() => dictionary.AddRange("Not allowed", null));
+            }
+
+            public Test Remove_key_ThrowsWhenKeyNull()
+            {
+                return Assert.Throws<ArgumentNullException>(() => dictionary.Remove(null));
+            }
+
+            public Test Remove_key_value_ThrowsWhenKeyNull()
+            {
+                return Assert.Throws<ArgumentNullException>(() => dictionary.Remove(null, "not null"));
+            }
         }
 
-        public class AfterSingleAdd
+
+        public class SingletonMultidictionary
         {
             private readonly Multidictionary<string, string> dictionary = new Multidictionary<string, string>();
             private readonly string key = "Hello";
             private readonly string value = "World";
-            public AfterSingleAdd()
+            public SingletonMultidictionary()
             {
                 dictionary.Add(key, value);
             }
 
-            public Test HasOneItem() => Assert.That(dictionary.Count).Is.EqualTo(1);
+            public Test HasEntryCountOne() => Assert.That(dictionary.Count).Is.EqualTo(1);
 
-            public Test HasSingleElement()
+            public Test HasKeyCountOne() => Assert.That(dictionary.Keys.Count).Is.EqualTo(1);
+
+            public Test HasAddedEntry()
             {
-                return Assert.That(dictionary.Single().Key).Is.EqualTo(key) &&
-                    Assert.That(dictionary.Single().Value).Is.SequenceEqualTo(value);
+                var entry = dictionary.Single();
+
+                return Assert.That(entry.Key).Is.EqualTo(key) &&
+                    Assert.That(entry).Is.SequenceEqualTo(value);
             }
 
-            public Test IndexerFetchesElement()
+            public Test ContainsSingleKey()
+            {
+                return Assert.That(dictionary.Keys)
+                    .Is.EquivalentTo(key);
+            }
+            
+            public Test HasAddedValue()
+            {
+                return Assert.That(dictionary.Single())
+                    .Is.SequenceEqualTo(value);
+            }
+
+            public Test IndexerReturnsSingleValue()
             {
                 return Assert.That(dictionary[key]).Is.SequenceEqualTo(value);
+            }
+
+            public Test RemoveExistingKey_LowersCount()
+            {
+                dictionary.Remove(key);
+
+                return Assert.That(dictionary.Count).Is.Zero;
+            }
+
+            public Test RemoveExistingKey_Succeeds()
+            {
+                return Assert.That(dictionary.Remove(key)).Is.True;
+            }
+        }
+
+
+        public class AddingDistinctItemsToSingleKey
+        {
+            private readonly Multidictionary<string, string> dictionary;
+
+            public AddingDistinctItemsToSingleKey()
+            {
+                dictionary = new Multidictionary<string, string>
+                {
+                    { "teamS", "#1" },
+                    { "teamS", "rules" },
+                    { "teamS", "cargo" }
+                };
+            }
+
+            public Test ContainsSingleKey()
+            {
+                return Assert.That(dictionary.Keys).Is.EquivalentTo("teamS");
+            }
+
+            public Test DoesNotIncreaseEntryCount() => Assert.That(dictionary.Count).Is.EqualTo(1);
+
+            public Test DoesNotIncreaseKeyCollectionCount()
+            {
+                return Assert.That(dictionary.Keys.Count).Is.EqualTo(1);
+            }
+
+            public Test GroupsValuesWithKey()
+            {
+                return Assert.That(dictionary["teamS"])
+                    .Is.EquivalentTo("rules", "#1", "cargo");
+            }
+
+            public Test IncreasesGroupSize()
+            {
+                return Assert.That(dictionary["teamS"].Count).Is.EqualTo(3);
+            }
+
+            public Test Remove_ExistingValue_ReturnsTrue()
+            {
+                return Assert.That(dictionary.Remove("teamS", "cargo")).Is.True;
+            }
+
+            public Test Remove_ExistingValue_RemovesValueFromGroup()
+            {
+                dictionary.Remove("teamS", "cargo");
+
+                return Assert.That(dictionary["teamS"]).Is.EquivalentTo("#1", "rules");
+            }
+        }
+
+        public class MultidictionaryWithItemsRemoved
+        {
+            private readonly Multidictionary<string, int> dictionary;
+
+            public MultidictionaryWithItemsRemoved()
+            {
+                dictionary = new Multidictionary<string, int>();
+                dictionary.AddRange("SUnit", new int[] { 50, 100, 150 });
+                dictionary.AddRange("hello", new int[] { 7 });
+                dictionary.AddRange("TeamS", new int[] { 420, 69 });
+
+                dictionary.Remove("hello", 7);
+
+            }
+
+            public IEnumerable<Test> HasNoEmptyGroupsWhenEnumerated()
+            {
+                return dictionary
+                    .Select(group => Assert.That(group).Is.Not.Empty);
+            }
+
+            public Test HasEntryCountUpdatedToReflectRemovedGroups()
+            {
+                return Assert.That(dictionary.Count).Is.EqualTo(2);
             }
         }
     }
--- Solutions/SUnitTestDrive/NewellClark.Collections/Multidictionary.cs
+++ Solutions/SUnitTestDrive/NewellClark.Collections/Multidictionary.cs
@@ -1,40 +1,232 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Linq;
 using System.Collections;
+using System.Diagnostics;
+using System.Linq;
 
 namespace NewellClark.Collections
 {
+    /// <summary>
+    /// Maps a single key to multiple values. Duplicates are allowed.
+    /// </summary>
+    /// <typeparam name="TKey">The key type. </typeparam>
+    /// <typeparam name="TValue">The value type.</typeparam>
     public class Multidictionary<TKey, TValue> 
-        : IEnumerable<KeyValuePair<TKey, IEnumerable<TValue>>>
+        : IEnumerable<Multidictionary<TKey, TValue>.Grouping>
     {
         private readonly Dictionary<TKey, List<TValue>> dictionary;
 
-        public Multidictionary(IEqualityComparer<TKey> comparer)
+        /// <summary>
+        /// Creates a new <see cref="Multidictionary{TKey, TValue}"/> that uses the specified
+        /// <see cref="IEqualityComparer{T}"/> to compare keys for equality.
+        /// </summary>
+        /// <param name="keyComparer">The <see cref="IEqualityComparer{T}"/> to use for containment checks of keys.</param>
+        public Multidictionary(IEqualityComparer<TKey> keyComparer)
         {
-            if (comparer is null) throw new ArgumentNullException(nameof(comparer));
+            if (keyComparer is null) throw new ArgumentNullException(nameof(keyComparer));
 
-            dictionary = new Dictionary<TKey, List<TValue>>(comparer);
+            dictionary = new Dictionary<TKey, List<TValue>>(keyComparer);
+            Keys = new KeyCollection(this);
         }
-        public Multidictionary() : this(EqualityComparer<TKey>.Default) { }
 
-        public int Count { get; private set; }
+        /// <summary>
+        /// Creates a new <see cref="Multidictionary{TKey, TValue}"/>.
+        /// </summary>
+        public Multidictionary() 
+            : this(EqualityComparer<TKey>.Default) { }
 
-        public void Add(TKey key, TValue value) => throw new NotImplementedException();
+        /// <summary>
+        /// Gets the number of entries in the <see cref="Multidictionary{TKey, TValue}"/>.
+        /// </summary>
+        public int Count => dictionary.Count;
 
-        public bool Remove(TKey key) => throw new NotImplementedException();
+        /// <summary>
+        /// Adds the specified value to the group with the specified key. If the key does not exist,
+        /// a new group is created. 
+        /// </summary>
+        /// <param name="key">The key of the group to add the value to.</param>
+        /// <param name="value">The value to add.</param>
+        public void Add(TKey key, TValue value)
+        {
+            if (!dictionary.TryGetValue(key, out List<TValue> list))
+                dictionary.Add(key, list = new List<TValue>());
+            list.Add(value);
+        }
 
-        public bool Contains(TKey key) => throw new NotImplementedException();
+        /// <summary>
+        /// Adds all the values in the collection to the gorup with the specified key.
+        /// </summary>
+        /// <param name="key">The key of the group to add to.</param>
+        /// <param name="values">The values to add.</param>
+        public void AddRange(TKey key, IEnumerable<TValue> values)
+        {
+            if (values is null) throw new ArgumentNullException(nameof(values));
 
-        public IEnumerable<TValue> this[TKey key] => throw new NotImplementedException();
+            if (!dictionary.TryGetValue(key, out List<TValue> list))
+                dictionary.Add(key, list = new List<TValue>());
 
-        public IEnumerator<KeyValuePair<TKey, IEnumerable<TValue>>> GetEnumerator()
+            list.AddRange(values);
+        }
+
+        /// <summary>
+        /// Removes the entry with the specified key, if it exists.
+        /// </summary>
+        /// <param name="key">The key of the entry to remove.</param>
+        /// <returns>True if the key was found and removed, false otherwise.</returns>
+        public bool Remove(TKey key)
+        {
+            bool removed = dictionary.Remove(key);
+
+            return removed;
+        }
+
+        /// <summary>
+        /// Removes the specified value from the group with the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value to remove.</param>
+        /// <returns>True if the value was successfully removed.</returns>
+        public bool Remove(TKey key, TValue value)
+        {
+            if (!dictionary.TryGetValue(key, out List<TValue> list))
+                return false;
+
+            bool wasRemoved = list.Remove(value);
+            if (list.Count == 0)
+                dictionary.Remove(key);
+
+            return wasRemoved;
+        }
+
+        /// <summary>
+        /// Indicates whether there is at least one value in the <see cref="Multidictionary{TKey, TValue}"/> 
+        /// associated with the specified key.
+        /// </summary>
+        /// <param name="key">The key to search for.</param>
+        /// <returns>True if the <see cref="Multidictionary{TKey, TValue}"/> contains the specified key.</returns>
+        public bool ContainsKey(TKey key) => dictionary.ContainsKey(key);
+
+        /// <summary>
+        /// Gets all the values associated with the specified key. If the key is not in the 
+        /// <see cref="Multidictionary{TKey, TValue}"/>, an empty group is returned.
+        /// </summary>
+        /// <param name="key">The key to search for.</param>
+        /// <returns>A group containing all the values associated with the key, or an empty group
+        /// if the key is not in the <see cref="Multidictionary{TKey, TValue}"/>.</returns>
+        public Grouping this[TKey key]
+        {
+            get => dictionary.ContainsKey(key) ?
+                new Grouping(key, dictionary[key]) :
+                default;
+        }
+
+        /// <summary>
+        /// Gets a collection of all the keys in the <see cref="Multidictionary{TKey, TValue}"/>.
+        /// </summary>
+        public KeyCollection Keys { get; }
+
+        /// <summary>
+        /// Gets an <see cref="IEnumerator{T}"/> that enumerates all the key-value groups in the 
+        /// <see cref="Multidictionary{TKey, TValue}"/>.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<Grouping> GetEnumerator()
         {
             foreach (var kvp in dictionary)
-                yield return new KeyValuePair<TKey, IEnumerable<TValue>>(kvp.Key, kvp.Value);
+                yield return new Grouping(kvp.Key, kvp.Value);
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+        /// <summary>
+        /// A read-only collection of all the keys in a <see cref="Multidictionary{TKey, TValue}"/>.
+        /// </summary>
+        public sealed class KeyCollection : IReadOnlyCollection<TKey>
+        {
+            private readonly Multidictionary<TKey, TValue> owner;
+
+            /// <summary>
+            /// Creates a new <see cref="KeyCollection"/> for the specified <see cref="Multidictionary{TKey, TValue}"/>.
+            /// </summary>
+            /// <param name="owner">The <see cref="Multidictionary{TKey, TValue}"/> that owns the 
+            /// newly-created <see cref="KeyCollection"/>.</param>
+            internal KeyCollection(Multidictionary<TKey, TValue> owner)
+            {
+                if (owner is null) throw new ArgumentNullException(nameof(owner));
+
+                this.owner = owner;
+            }
+
+            /// <summary>
+            /// Gets the number of keys in the <see cref="KeyCollection"/>.
+            /// </summary>
+            public int Count => owner.Count;
+
+            /// <summary>
+            /// Gets an <see cref="IEnumerator{T}"/> that enumerates the <see cref="KeyCollection"/>.
+            /// </summary>
+            /// <returns>An <see cref="IEnumerator{T}"/> for the current <see cref="KeyCollection"/>.</returns>
+            public IEnumerator<TKey> GetEnumerator()
+            {
+                foreach (var entry in owner)
+                    yield return entry.Key;
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        /// <summary>
+        /// A group containing all the values associated with a particular key in a <see cref="Multidictionary{TKey, TValue}"/>.
+        /// </summary>
+        public struct Grouping : IGrouping<TKey, TValue>, IReadOnlyCollection<TValue>
+        {
+            private readonly IReadOnlyCollection<TValue> items;
+
+            /// <summary>
+            /// Creates an empty <see cref="Grouping"/> for the specified key.
+            /// </summary>
+            /// <param name="key">The key of the <see cref="Grouping"/>.</param>
+            internal Grouping(TKey key)
+            {
+                this.Key = key;
+                this.items = null;
+            }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="key"></param>
+            /// <param name="items"></param>
+            internal Grouping(TKey key, IReadOnlyCollection<TValue> items)
+            {
+                Debug.Assert(items != null);
+
+                this.Key = key;
+                this.items = items;
+            }
+
+            /// <summary>
+            /// Gets the number values in the <see cref="Grouping"/>.
+            /// </summary>
+            public int Count => items?.Count ?? 0;
+
+            /// <summary>
+            /// Gets the key associated with the <see cref="Grouping"/>.
+            /// </summary>
+            public TKey Key { get; }
+
+            /// <summary>
+            /// Gets an enumerator for the <see cref="Grouping"/>.
+            /// </summary>
+            /// <returns></returns>
+            public IEnumerator<TValue> GetEnumerator()
+            {
+                return items?.GetEnumerator() ??
+                    Enumerable.Empty<TValue>().GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
     }
 }
