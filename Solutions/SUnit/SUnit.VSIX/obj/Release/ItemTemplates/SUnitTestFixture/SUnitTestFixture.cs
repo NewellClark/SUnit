a6d20cf122804b13e54f65b1633e22db6c451cca
Newell Clark <newellofthefuture@gmail.com>
2020-03-21 18:00:03 -0400

    Added icon to SUnit project template.

---
+++ Solutions/SUnit/SUnit.VSIX/obj/Release/ItemTemplates/SUnitTestFixture/SUnitTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SUnit;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$
+    {
+        public Test TestMethod()
+        {
+            return Test.Fail;
+        }
+    }
+}
